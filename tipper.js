8b50723650abee8b17058fd884614022ae7a4d50
Bernd Kohler <bernd.kohler@mailfence.com>
2021-10-28 11:54:14 +0200

    init

---
+++ tipper.js
@@ -0,0 +1,34 @@
+function tipKohl(address) {
+  if(document.ethereum === undefined) {
+    alert("No wallet detected")
+    return
+  }
+
+  console.log("Tipping " +  address)
+}
+
+function addTipButtons() {
+  const subjects = document.getElementsByClassName("labelSubject")
+  for(i=0; i<subjects.length; i++) {
+    console.log("Replacing " + i)
+    const s = subjects[i];
+    const addr = s.innerText 
+    if(addr.startsWith("0x")) {
+      const btn = document.createElement('button')
+	  btn.innerText = "Tip"
+	  btn.addEventListener('click', () => {tipKohl(addr)})
+	  s.replaceWith(btn)
+    }
+  }
+}
+
+window.onload = (evt) => {
+  addTipButtons()
+  const posts = document.getElementsByClassName("divPosts")[0]
+  const observer = new MutationObserver((mutationList, observer) => {
+    console.log("mutation")
+    addTipButtons()
+  })
+  observer.observe(posts, {childList: true, attributes: false})
+}
+
