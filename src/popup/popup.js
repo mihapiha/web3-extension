1eadf6806a581b83b6eb7eadfe391273812361da
Bernd Kohler <bernd.kohler@mailfence.com>
2021-11-10 16:42:17 +0100

    Popup redesign with buttons for donations

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,8 @@
 const btn = document.getElementById("save")
 const valInput = document.getElementById("tipValue")
+const imgURL = chrome.extension.getURL("icon/kohl-k2-32.png");
+document.getElementById("cabbage-icon").src = imgURL;
+
 
 chrome.runtime.sendMessage({type: "KOHL_TIP_QUERY"}, (resp) => {
   valInput.value = resp.value
